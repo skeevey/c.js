1359d234b97a00111afc6eebbb71b3ff0f9848e3
Samuel Reed <samuel.trace.reed@gmail.com>
2014-05-05 16:22:23 +0800

    Add c.ab2ipcstr and c.ipcstr2ab

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+'use strict';
 module.exports = {
   serialize: require('./lib/serialize'),
-  deserialize: require('./lib/deserialize')
+  deserialize: require('./lib/deserialize'),
+  ab2ipcstr: function(buffer) {
+    var str = "";
+    var bufferView = new Uint8Array(buffer);
+    for(var i = 0; i < bufferView.byteLength; i++){
+      str += (bufferView[i]+0x100).toString(16).slice(-2);
+    }
+    return str;
+  },
+  ipcstr2ab: function(str) {
+    var buffer = new ArrayBuffer(str.length/2);
+    var bufferView = new Uint8Array(buffer);
+    for(var i = 0; i < buffer.byteLength; i++){
+      bufferView[i] = parseInt("0x"+str.substr(2*i,2));
+    }
+    return buffer;
+  }
 };
