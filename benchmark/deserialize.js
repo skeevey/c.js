55db67b13d680de2b23693bce92f2fbc6b67d41f
Samuel Reed <samuel.trace.reed@gmail.com>
2015-12-15 15:04:18 -0600

    Refactor benchmarks.

--- benchmark/deserialize.js
+++ benchmark/deserialize.js
@@ -1,28 +1,19 @@
 'use strict';
 
-const deserialize = require('../lib/deserialize');
-const serialize = require('../lib/serialize');
-const ITERATIONS = 1000;
-const BENCH_ITERATIONS = 10;
-let benchIters = 0;
+const bench = require('./_bench');
+const deserialize = require('../src/deserialize');
+const serialize = require('../src/serialize');
 
 console.log('KDB Deserialize benchmark.');
 
 const largeObject = serialize(require('./data.json'));
 
-// Benchmark some of the message parsing functions.
-function run() {
-  let key = 'deserializing data.json ' + ITERATIONS + ' times:';
-  console.time(key);
-  for (let i = 0; i < ITERATIONS; i++) {
-    deserialize(largeObject);
-  }
-  console.timeEnd(key);
-  benchIters++;
-  if (benchIters < BENCH_ITERATIONS) return run();
-  console.log('---------');
-  process.exit(0);
-}
-
-run();
-
+bench([
+  {
+    iterations: 1000,
+    name: 'Deserializing data.json',
+    fn: function() {
+      deserialize(largeObject);
+    }
+  },
+]);
--- benchmark/serialize.js
+++ benchmark/serialize.js
@@ -1,26 +1,42 @@
 'use strict';
 
-const serialize = require('../lib/serialize');
-const ITERATIONS = 1000;
-const BENCH_ITERATIONS = 10;
-let benchIters = 0;
+const bench = require('./_bench');
+const serialize = require('../src/serialize');
 
 console.log('KDB Serialize benchmark.');
 
 const largeObject = require('./data.json');
+const smallArray = [1,2,3,4,5];
+const smallStr = 'This is a string.';
+const smallObj = {foo: 'bar'};
 
-// Benchmark some of the message parsing functions.
-function run() {
-  let key = 'serializing data.json ' + ITERATIONS + ' times:';
-  console.time(key);
-  for (let i = 0; i < ITERATIONS; i++) {
-    serialize(largeObject);
-  }
-  console.timeEnd(key);
-  benchIters++;
-  if (benchIters < BENCH_ITERATIONS) return run();
-  console.log('---------');
-  process.exit(0);
-}
-
-run();
+bench([
+  {
+    iterations: 1000,
+    name: 'Serializing data.json',
+    fn: function() {
+      serialize(largeObject);
+    }
+  },
+  {
+    iterations: 100000,
+    name: 'Serializing small array',
+    fn: function() {
+      serialize(smallArray);
+    }
+  },
+  {
+    iterations: 100000,
+    name: 'Serializing small string',
+    fn: function() {
+      serialize(smallStr);
+    }
+  },
+  {
+    iterations: 100000,
+    name: 'Serializing small object',
+    fn: function() {
+      serialize(smallObj);
+    }
+  },
+]);
