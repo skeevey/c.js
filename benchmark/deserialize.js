00d2e0b2b2301b5d2ab62387bd5a160925e2214f
Samuel Reed <samuel.trace.reed@gmail.com>
2015-12-15 11:07:18 -0600

    Add deserialize benchmark.

---
+++ benchmark/deserialize.js
@@ -0,0 +1,45 @@
+'use strict';
+
+require("babel-register"); // for now, TODO build script and include
+var deserialize = require('../lib/deserialize');
+var serialize = require('../lib/serialize');
+var ITERATIONS = 500;
+var OBJ_SIZE = 10000;
+var BENCH_ITERATIONS = 10;
+var benchIters = 0;
+var empty = function(){};
+
+console.log('KDB Deserialize benchmark.');
+
+var largeObject = serialize(generateObjectOfSize(10000));
+
+// Benchmark some of the message parsing functions.
+function run() {
+  var key = 'deserializing obj of size ' + OBJ_SIZE + ', ' + ITERATIONS + ' times:';
+  console.time(key);
+  for (var i = 0; i < ITERATIONS; i++) {
+    deserialize(largeObject);
+  }
+  console.timeEnd(key);
+  benchIters++;
+  if (benchIters < BENCH_ITERATIONS) return run();
+  console.log('---------');
+  process.exit(0);
+}
+
+run();
+
+
+// Generate a large object.
+// Size is in bytes; e.g. a single key object serializes to
+// '{"a":"s"}' which is 9 bytes. Each subsequent key is 8 bytes
+// due to the comma.
+// Not really accurate because the keys get larger when the numbers go up but,
+// it doesn't really matter for this test.
+function generateObjectOfSize(size) {
+  var obj = {};
+  for (var i = 1; i < size; i += 8) {
+    obj[i] = "s";
+  }
+  return obj;
+}
